113109836317c2fb690004dc6908c28bcfe3e8cb
阿远 <130144577@qq.com>
2022-12-28 20:59:06 +0800

    1

--- internal/service/Tools.go
+++ internal/service/Tools.go
@@ -33,7 +33,7 @@ func UpdateGoodsProportion(goodsId int, cat_type int) {
 		config := myDao.GetOneSystemConfig(1)
 		if cat_type == 2 && goodsInfo.Proportion >= config.BotProportion && goodsInfo.SteamSellPrice >= config.BotPrice {
 			//商品名称是否包含 印花
-			if !util.Contains(goodsInfo.Name, "印花") {
+			if !util.Contains(goodsInfo.MarketHashName, "Sticker") {
 				sendTelegram(goodsInfo)
 			}
 		}
